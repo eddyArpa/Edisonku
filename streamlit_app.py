69e39fee635a84806d8a7d4214d7e3bfe840cf04
eddyArpa <edisonmarpaung@gmail.com>
2024-06-28 11:19:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,32 @@
 # %%writefile  webapp.py 
-# import streamlit as st 
-
-# def main() : 
-#   st.write('Minimal Example By Edison Marpaung')
-
-#   st.header('This is Header')
-#   st.subheader('This is SubHeader')
-#   st.markdown('# Rendering Markdown ')
-#   st.write('Some Phytagorean Equation : ')
-#   st.latex('c^2 = a^2+b^2')
-
-# if __name__ == '__main__' : 
-#   main()
-
 
 import streamlit as st 
-import pandas as pd 
-import requests
-from st_aggrid import AgGrid
+# import pandas as pd 
+# import requests
+# from st_aggrid import AgGrid
 #baca dataframe dari file csv 
-titanic = pd.read_csv('house_clean.csv')
-#read json file dari data covid 
-flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
-  "month_limit": "2020-01-01",
-  "window_size": 12
-}).json()
+house = pd.read_csv('house_clean.csv')
+# #read json file dari data covid 
+# flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
+#   "month_limit": "2020-01-01",
+#   "window_size": 12
+# }).json()
 
 def main() : 
+
+  st.write('Minimal Example By Edison Marpaung')
+
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
+
+  
   st.write('Contoh dataframe')
-  st.dataframe(titanic)
-  st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(titanic)
-  st.table([x for x in range(1,5)])
+  st.dataframe(house)
+
+  
+
 if __name__ == '__main__' : 
   main()
